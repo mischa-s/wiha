c1c1581034c7ea5e427640b4bcbf41ef20c3a616
mischa saunders <mischa.saunders@gmail.com>
2020-01-14 15:02:01 +1300

    add wiha color scheme

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -16,9 +16,9 @@ export default class BlogIndexPage extends React.Component {
           <h1
             className="has-text-weight-bold is-size-1"
             style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
-              color: 'white',
+              boxShadow: '0.5rem 0 0 #FDF100, -0.5rem 0 0 #FDF100',
+              backgroundColor: '#FDF100',
+              color: 'black',
               padding: '1rem',
             }}
           >
