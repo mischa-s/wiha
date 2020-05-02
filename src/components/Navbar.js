f8a9bc8905699037c7833e05824adde819cb5fd8
Mischa Saunders <mischa.saunders@ackama.com>
2020-05-02 23:32:48 +1200

    add mobile nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,68 @@
 import React from "react";
-import { Link } from "gatsby";
-import logo from "../img/logo.png";
-import styled from "@emotion/styled";
-import { Button } from "@chakra-ui/core";
 import {
+  IconButton,
+  PseudoBox,
+  Button,
+  Stack,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionPanel,
+  AccordionIcon,
 } from "@chakra-ui/core";
+import styled from "@emotion/styled";
+import { Link } from "gatsby";
 
+import logo from "../img/logo.png";
+
+import theme from "../theme/theme";
+const { breakpoints } = theme;
+
+const menuItems = [
+  {
+    link: "/play",
+    label: "Play",
+  },
+  {
+    link: "/about",
+    label: "About",
+  },
+  {
+    link: "/blog",
+    label: "Blog",
+  },
+  {
+    link: "/contact",
+    label: "Contact",
+  },
+  {
+    Leagues: [
+      {
+        link: "/frozen",
+        label: "Frozen Fours",
+      },
+      {
+        link: "/bear",
+        label: "Bear League",
+      },
+      {
+        link: "/youth",
+        label: "Youth",
+      },
+    ],
+  },
+];
 
 const NavbarWrapper = styled.div`
   background-color: black;
@@ -41,37 +94,148 @@ const NavItems = styled.div`
   justify-content: flex-end;
 `;
 
+const MobileNavWrapper = styled.div`
+  @media (min-width: ${breakpoints[1]}) {
+    display: none;
+  }
+`;
+
+const DesktopNavWrapper = styled.div`
+  display: none;
+  @media (min-width: ${breakpoints[1]}) {
+    display: initial;
+  }
+`;
+
 export default function Navbar() {
   return (
     <nav role="navigation" aria-label="main-navigation">
       <NavbarWrapper>
         <NavBarContent>
-          <div>
-            <Link to="/" title="Logo">
-              <NavLogo src={logo} alt="WIHA" />
-            </Link>
-          </div>
-          <div id="navMenu">
-            <NavItems>
-              <Menu>
-                <MenuButton variantColor={"blackAlpha"} size="lg" as={Button} rightIcon="chevron-down">
-                  Leagues
-                </MenuButton>
-                <MenuList >
-                  <Link to={"/frozen"}><MenuItem >Frozen Fours</MenuItem></Link>
-                  <Link to={"/bear"}><MenuItem >Bear League</MenuItem></Link>
-                  <Link to={"/youth"}><MenuItem >Youth</MenuItem></Link>
-                </MenuList>
-              </Menu>
-              <NavButton link={"/play"} text={"Play"} />
-              <NavButton link={"/about"} text={"About"} />
-              <NavButton link={"/blog"} text={"Blog"} />
-              <NavButton link={"/contact"} text={"Contact"} />
-            </NavItems>
-          </div>
+          <Link to="/" title="Logo">
+            <NavLogo src={logo} alt="WIHA" />
+          </Link>
+          <DesktopNav />
+          <MobileNav />
         </NavBarContent>
       </NavbarWrapper>
     </nav>
+  );
+}
+
+function MobileNav() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
+  return (
+    <MobileNavWrapper>
+      <IconButton
+        ref={btnRef}
+        variantColor="blackAlpha"
+        fontSize="30px"
+        onClick={onOpen}
+        aria-label="Search database"
+        icon="hamburger"
+      />
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent >
+          <DrawerCloseButton />
+          <DrawerHeader />
+
+          <DrawerBody>
+            <Stack>
+              {menuItems.map((item) => {
+                if (!item.link) {
+                  return <MobileAccordionItem items={item} />;
+                }
+                return <MobileNavItem item={item} />;
+              })}
+            </Stack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </MobileNavWrapper>
+  );
+}
+
+function MobileNavItem({ item }) {
+  const { link, label } = item;
+  return (
+    <>
+      <Link to={link}>
+        <PseudoBox
+          p="5"
+          w="100%"
+          transition="all 0.2s"
+          _focus={{ boxShadow: "outline" }}
+          _hover={{ bg: "blackAlpha.50" }}
+        >
+          {label}
+        </PseudoBox>
+      </Link>
+    </>
+  );
+}
+
+function MobileAccordionItem({ items }) {
+  const category = Object.keys(items)[0];
+
+  return (
+    <Accordion allowMultiple>
+      <AccordionItem defaultIsOpen={false}>
+        <AccordionHeader>
+          <PseudoBox my="5" flex="1" textAlign="left">
+            {category}
+          </PseudoBox>
+          <AccordionIcon />
+        </AccordionHeader>
+        <AccordionPanel pb={4}>
+          {items[category].map((item) => {
+            return <MobileNavItem item={item} />;
+          })}
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  );
+}
+
+function DesktopNav() {
+  return (
+    <DesktopNavWrapper>
+      <NavItems>
+        <Menu>
+          <MenuButton
+            variantColor={"blackAlpha"}
+            size="lg"
+            as={Button}
+            rightIcon="chevron-down"
+          >
+            Leagues
+          </MenuButton>
+          <MenuList>
+            <Link to={"/frozen"}>
+              <MenuItem>Frozen Fours</MenuItem>
+            </Link>
+            <Link to={"/bear"}>
+              <MenuItem>Bear League</MenuItem>
+            </Link>
+            <Link to={"/youth"}>
+              <MenuItem>Youth</MenuItem>
+            </Link>
+          </MenuList>
+        </Menu>
+        <NavButton link={"/play"} text={"Play"} />
+        <NavButton link={"/about"} text={"About"} />
+        <NavButton link={"/blog"} text={"Blog"} />
+        <NavButton link={"/contact"} text={"Contact"} />
+      </NavItems>
+    </DesktopNavWrapper>
   );
 }
 
