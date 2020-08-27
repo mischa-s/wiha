5493dc89609a71776bac5c6414797d11573ec06a
Mischa Saunders <mischa.saunders@gmail.com>
2020-08-27 18:11:59 +1200

    add styles for markdown table

--- src/lib/markdown-renderers.js
+++ src/lib/markdown-renderers.js
@@ -1,5 +1,27 @@
 import React from "react";
 import { List, ListItem } from "@chakra-ui/core";
+import styled from "@emotion/styled";
+
+const Table = styled.table`
+  width: 50em;
+  max-width: 100%;
+  overflow-x: scroll;
+  margin: 1rem 0;
+
+  th {
+    background: rgba(0, 0, 0, 0.8);
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    color: #fef001;
+    text-transform: uppercase;
+  }
+
+  th,
+  td {
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    padding: 1rem;
+  }
+`;
+
 
 export default {
   list: function ChakraList(props) {
@@ -8,4 +30,7 @@ export default {
   listItem: function ChakraListItem(props) {
     return <ListItem>{props.children}</ListItem>;
   },
+  table: function StyledTable(props) {
+    return <Table>{props.children}</Table>
+  }
 };
